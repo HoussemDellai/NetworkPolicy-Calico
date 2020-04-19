2846265803563a60de3f033f362b0f38a12d1e0d
Houssem Dellai <houssem.dellai@live.com>
2020-04-19 13:30:07 +0200

    edited doc

--- commands.sh
+++ commands.sh
@@ -1,11 +1,13 @@
-# 1. Create development namespace
-kubectl create -f 1-namespace
+# 0. Setting up AKS with Calico enabled
+
+# 1. Create development namespace with labels
+kubectl create -f 1-namespace-development.yaml
 # or
 kubectl create namespace development
 kubectl label namespace/development purpose=development
 
 # 2. Create an nginx Pod and Service
-kubectl create -f 2-pod-svc
+kubectl create -f 1-pod-svc-nginx-backend.yaml
 # or
 kubectl run backend --image=nginx --labels app=webapp,role=backend --namespace development --expose --port 80 --generator=run-pod/v1
 
@@ -24,7 +26,38 @@ kubectl apply -f 4-network-policy-deny.yaml
 kubectl exec alpine -n development -- wget -qO- --timeout=2 http://backend
 # or
 kubectl run --rm -it --image=alpine network-policy --namespace development --generator=run-pod/v1
+wget -qO- --timeout=2 http://backend
 
 # 5. Allow inbound traffic based on a pod label
 # Update the previous Network Policy to allow traffic from only pods with specific labels
 kubectl apply -f 5-network-policy-allow-pod.yaml
+# 5.1 test
+kubectl create -f 3-pod-alpine
+kubectl exec alpine -n development -- wget -qO- http://backend
+# or
+kubectl run --rm -it frontend --image=alpine --labels app=webapp,role=frontend --namespace development --generator=run-pod/v1
+wget -qO- http://backend
+
+# 5.2 test
+# or 
+kubectl run --rm -it --image=alpine network-policy --namespace development --generator=run-pod/v1
+wget -qO- --timeout=2 http://backend
+
+# 6 Allow traffic only from within a defined namespace
+# 6.1 Test without Policy and pod reaching other namespaces
+kubectl create namespace production
+kubectl label namespace/production purpose=production
+kubectl run --rm -it frontend --image=alpine --labels app=webapp,role=frontend --namespace production --generator=run-pod/v1
+wget -qO- http://backend.development
+
+# 6.2 Create the policy
+kubectl apply -f 3-network-policy
+
+# 6.3 Test with policy and pod from same namespace
+kubectl run --rm -it frontend --image=alpine --labels app=webapp,role=frontend --namespace production --generator=run-pod/v1
+wget -qO- --timeout=2 http://backend.development
+
+# 6.4 Test with policy and pod from different namespace
+kubectl run --rm -it frontend --image=alpine --labels app=webapp,role=frontend --namespace development --generator=run-pod/v1
+wget -qO- http://backend
+
